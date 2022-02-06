d8ddd5f8019b7dd5b22fb2c2be5895efd541e621
pinkphone <70506265+pinkphone1818@users.noreply.github.com>
2022-02-06 22:09:32 +0800

    Update Program.cs

--- PrismBot/Program.cs
+++ PrismBot/Program.cs
@@ -39,7 +39,7 @@ namespace PrismBot
 
             await client.SetStatusAsync(UserStatus.Idle);
 
-            await client.LoginAsync(TokenType.Bot, "OTM5NDg5NjE5MDE4NDAzODYw.Yf5l5A.7k7Mxk9B5KainxTxvXxJgZ1OmUY");
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
             await client.StartAsync();
 
             await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
