d5483b8fc937fdd43e73f37ea8ad707bed95d0a5
chash <70506265+cchash@users.noreply.github.com>
2022-03-07 14:36:58 +0800

    Add files via upload

---
+++ Prismos/Modules/Misc.cs
@@ -0,0 +1,95 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.Rest;
+using Discord.WebSocket;
+
+namespace Prismos.Modules
+{
+    public class Misc : InteractionModuleBase<SocketInteractionContext<SocketSlashCommand>>
+    {
+        [SlashCommand("help", "Get bot help manual.")]
+        public async Task Help()
+        {
+            await Context.Interaction.DeferAsync(true);
+
+            EmbedBuilder miscembed = new();
+            miscembed.WithColor(Color.Blue);
+            miscembed.WithTitle("Misc Commands");
+            miscembed.AddField("/say", "Make the bot say something.");
+            miscembed.AddField("/avatar {user}", "Returns user's avatar.");
+
+            EmbedBuilder modembed = new();
+            modembed.WithColor(Color.Orange);
+            modembed.WithTitle("Moderation Commands");
+            modembed.AddField("/kick {user}", "Kicks user.");
+            modembed.AddField("/ban {user}", "Bans user.");
+
+            EmbedBuilder funembed = new();
+            funembed.WithColor(Color.Green);
+            funembed.WithTitle("Fun Commands");
+            funembed.AddField("/joke", "Returns random joke.");
+            funembed.AddField("/reddit {subreddit}", "Grabs a random Reddit post from a specified subreddit.");
+            funembed.AddField("/fight {user}", "Challenge someone to a battle.");
+            funembed.AddField("/meme", "Returns a random meme.");
+            funembed.AddField("/wholesome", "Returns a random wholesome meme.");
+            funembed.AddField("/kitty", "Returns a random image of a cat.");
+            funembed.AddField("/bunny", "Returns a random image of a rabbit.");
+            funembed.AddField("/cute", "Returns a random cute image.");
+            funembed.AddField("/copypasta", "Returns a random copypasta.");
+            funembed.AddField("/greentext", "Returns a random 4chan greentext from...Reddit. I'm too lazy to make a 4chan web scraper.");
+            funembed.AddField("/hornylevel {user}", "Returns horny level of {user}. If {user} is empty, returns horny level of message author.");
+            funembed.AddField("/chadlevel {user}", "Returns chad level of {user}. If {user} is empty, returns chad level of message author.");
+            funembed.AddField("/hornylist", "Returns the top 10 horniest people in the server.");
+            funembed.AddField("/chadlist", "Returns the top 10 chaddest people in the server.");
+
+            EmbedBuilder nsfwembed = new();
+            nsfwembed.WithColor(Color.DarkRed);
+            nsfwembed.WithTitle("NSFW Commands");
+            nsfwembed.AddField("/nsfw", "Returns a random NSFW image. This command can only be used in an NSFW channel.");
+            nsfwembed.AddField("/hentai", "Returns a random hentai image. This command can only be used in an NSFW channel.");
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embeds = new Embed[] { miscembed.Build(), modembed.Build(), funembed.Build(), nsfwembed.Build() });
+        }
+
+        [SlashCommand("say", "Make the bot say something.")]
+        public async Task Say(string message)
+        {
+            await Context.Interaction.DeferAsync();
+
+            if (message.Contains("@everyone") || message.Contains("@here"))
+            {
+                await RespondAsync("You think you're being smart huh? You can't get me to ping everyone.");
+                return;
+            }
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Content = message);
+        }
+
+        [SlashCommand("avatar", "Get user's avatar.")]
+        public async Task Avatar(string? user = null)
+        {
+            await Context.Interaction.DeferAsync();
+
+            SocketUser user_;
+            if (user == null) user_ = Context.User;
+            else if (user[0] == '<' && user[user.Length - 1] == '>')
+            {
+                await Context.Guild.DownloadUsersAsync();
+                user_ = Context.Guild.GetUser(ulong.Parse(user.Trim('<', '>').Remove(0, 2)));
+            }
+            else
+            {
+                await Context.Guild.DownloadUsersAsync();
+                user_ = Context.Guild.GetUser(ulong.Parse(user));
+            }
+
+            System.Random rng = new();
+
+            EmbedBuilder embed = new();
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithImageUrl(user_.GetAvatarUrl(ImageFormat.Auto, 640));
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+    }
+}
---
+++ Prismos/Modules/Reddit.cs
@@ -0,0 +1,202 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Prismos.Services;
+
+namespace Prismos.Modules
+{ 
+    public class Reddit : InteractionModuleBase<SocketInteractionContext<SocketSlashCommand>>
+    {
+        public RedditService? rService { get; set; }
+
+        private string[] meme_subreddits = new string[] { "memes", "dankmemes" };
+        private string[] nsfw_subreddits = new string[] { "nsfw", "toocuteforporn" };
+
+        [SlashCommand("reddit", "Get a Reddit post.")]
+        public async Task Reddit_(string subreddit)
+        {
+            await Context.Interaction.DeferAsync();
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetAny(subreddit);
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithUrl(selected.RedditUrl);
+            embed.WithTitle(selected.Title);
+            embed.WithDescription(selected.SelfText);
+            embed.WithImageUrl(selected.Url);
+            embed.WithFooter($"Posted by u/{selected.Author}");
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+
+        [SlashCommand("kitty", "Get a picture of a cat.")]
+        public async Task Kitty()
+        {
+            await Context.Interaction.DeferAsync();
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetImages("cats");
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithUrl(selected.RedditUrl);
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithImageUrl(selected.Url);
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+
+        [SlashCommand("bunny", "Bunny pics!")]
+        public async Task Bunny()
+        {
+            await Context.Interaction.DeferAsync();
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetImages("rabbits");
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithUrl(selected.RedditUrl);
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithImageUrl(selected.Url);
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+
+        [SlashCommand("cute", "Get a cute image.")]
+        public async Task Cute()
+        {
+            await Context.Interaction.DeferAsync();
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetImages("cute");
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithUrl(selected.RedditUrl);
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithImageUrl(selected.Url);
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+
+        [SlashCommand("meme", "Get a cool meme.")]
+        public async Task Meme()
+        {
+            await Context.Interaction.DeferAsync();
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetImages(meme_subreddits[rng.Next(0, meme_subreddits.Length - 1)]);
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithUrl(selected.RedditUrl);
+            embed.WithTitle(selected.Title);
+            embed.WithImageUrl(selected.Url);
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+
+        [SlashCommand("wholesome", "Get a wholesome meme.")]
+        public async Task Wholesome()
+        {
+            await Context.Interaction.DeferAsync();
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetImages("wholesomememes");
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithUrl(selected.RedditUrl);
+            embed.WithTitle(selected.Title);
+            embed.WithImageUrl(selected.Url);
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+
+        [SlashCommand("greentext", "Get a 4chan greentext.")]
+        public async Task Greentext()
+        {
+            await Context.Interaction.DeferAsync();
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetImages("greentext");
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithUrl(selected.RedditUrl);
+            embed.WithTitle(selected.Title);
+            embed.WithImageUrl(selected.Url);
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+
+        [SlashCommand("copypasta", "Get a copypasta.")]
+        public async Task Copypasta()
+        {
+            await Context.Interaction.DeferAsync();
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetAny("copypasta");
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithUrl(selected.RedditUrl);
+            embed.WithTitle(selected.Title);
+            embed.WithDescription(selected.SelfText);
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+
+        [SlashCommand("nsfw", "Get an NSFW image")]
+        public async Task NSFW()
+        {
+            await Context.Interaction.DeferAsync();
+
+            if (Context.Channel is ITextChannel tc && !tc.IsNsfw)
+            {
+                await Context.Interaction.ModifyOriginalResponseAsync(m => m.Content = "You can only use this command in an NSFW channel!");
+                return;
+            }
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetImages(nsfw_subreddits[rng.Next(0, nsfw_subreddits.Length - 1)], true);
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithImageUrl(selected.Url);
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+
+        [SlashCommand("hentai", "Get a hentai image.")]
+        public async Task Hentai()
+        {
+            await Context.Interaction.DeferAsync();
+
+            if (Context.Channel is ITextChannel tc && !tc.IsNsfw)
+            {
+                await Context.Interaction.ModifyOriginalResponseAsync(m => m.Content = "You can only use this command in an NSFW channel!");
+                return;
+            }
+
+            System.Random rng = new();
+            RedditPost[] posts = await rService.GetImages("hentai", true);
+            RedditPost selected = posts[rng.Next(0, posts.Length - 1)];
+
+            EmbedBuilder embed = new();
+            embed.WithColor(rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+            embed.WithImageUrl(selected.Url);
+
+            await Context.Interaction.ModifyOriginalResponseAsync(m => m.Embed = embed.Build());
+        }
+    }
+}
